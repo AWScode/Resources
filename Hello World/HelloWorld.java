0eed45e42556b1c0298b46c57563bb701469a9e5
Zack Considine <zack_considine@m013391.PK5001Z>
2017-08-27 12:09:24 -0700

    Added Hello World to Resources

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,9 @@
+/*The classic Hello World java executable.
+This is a demonstration of the text palette in Atom
+as well as the sample for a class and main method */
+
+public class HelloWorld {
+    public static void main (String [] args) {
+        System.out.println("Hello World!");
+    }
+}

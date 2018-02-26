736a69b5ac4ea66fb1ac079422b8d4eec5178bc0
Zack Considine <zack_considine@m013391.aw.org>
2018-02-26 13:33:02 -0800

    ConnectFour Game for 2/26/18

---
+++ Game/ConnectFour.java
@@ -0,0 +1,19 @@
+/* ConnectFour.java by Mr. Considine
+This class holds all of the data and methods
+to create ConnectFour games. This is a class to be
+imported and used in other files that want to play
+this game. */
+
+public class ConnectFour {
+
+  //Instance Variables here
+
+  public ConnectFour() {
+    //Constructor Method here
+  }
+
+  // Get and Set Methods here
+
+
+  // Other methods here
+}
---
+++ Game/Game.java
@@ -0,0 +1,27 @@
+/* Game.java by Mr. Considine
+This class holds all of the data and methods
+to play a game of ConnectFour. */
+
+public class Game {
+
+  public static void main(String[] args) {
+    // main method here
+
+
+    //create a ConnectFour board
+
+
+    // prompt player1 to place a piece
+
+
+    //Display board
+
+
+    // prompt player2 to place a piece
+
+
+    // Display board and repeat
+  }
+
+  // method for displaying the board
+}

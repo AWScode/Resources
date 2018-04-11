4b5fef3d9f50a2c35a134ccd83a1f01848db3799
Zack Considine <zack_considine@m013391.aw.org>
2018-04-11 11:46:21 -0700

    BinaryTree added

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,32 @@
+/* BinaryTree.java created by Mr. Considine
+This program holds all of the information for
+the BinaryTree class. BinaryTree stores data in a
+specific data structure that makes adding data
+and searching for data possible in O(logn) time */
+import java.util.*;
+import java.io.*;
+
+public class BinaryTree {
+
+  public Node root;
+
+  public BinaryTree(Node root) {
+    this.root = root;
+
+  }
+
+  public boolean searchNode(Int val) {
+
+  }
+
+  public void addNode(Int val) {
+
+  }
+
+  public void removeNode(Int val) {
+
+  }
+
+  public static void main(String[] args) {
+    
+  }
---
+++ Trees/Node.java
@@ -0,0 +1,31 @@
+/* Node.java created by Mr. Considine
+This class holds all of the information
+and methods responsible for the Node for
+a BinaryTree. */
+
+import java.util.*;
+import java.io.*;
+
+public class Node {
+
+  public Node parent;
+  public Node child1;
+  public Node child2;
+
+  public Node(Int val) {
+
+  }
+
+  public Node getParent() {
+
+  }
+
+  public void setChild() {
+
+  }
+
+  public Node getChild() {
+    
+  }
+
+}

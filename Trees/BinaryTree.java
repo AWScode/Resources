15eb0b0a6324e24350ed0b796471ce6078b80025
Zack Considine <zack_considine@m013391.aw.org>
2018-04-13 10:56:52 -0700

    All of Node and most of BTree

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -10,23 +10,51 @@ public class BinaryTree {
 
   public Node root;
 
-  public BinaryTree(Node root) {
-    this.root = root;
+  public BinaryTree(int val) {
+    Node root1 = new Node(val);
+    this.root = root1;
+  }
+
+  public Node getRoot() {
+    return this.root;
+  }
+
+  public boolean searchNode(int val) {
+    return false;
+  }
+
+  public void addNode(int val) {
+  }
+
+  public void removeNode(int val) {
 
   }
 
-  public boolean searchNode(Int val) {
-
-  }
-
-  public void addNode(Int val) {
-
-  }
-
-  public void removeNode(Int val) {
-
+  public void printTree(Node root) {
+    if (root != null) {
+      String valS = Integer.toString(root.getValue());
+      System.out.println("[" + valS + "]");
+      this.printTree(root.getChild(false));
+      this.printTree(root.getChild(true));
+    }
   }
 
   public static void main(String[] args) {
-    
+
+    BinaryTree tree1 = new BinaryTree(10);
+
+    /*tree1.addNode(8);
+    tree1.addNode(12);
+    tree1.addNode(14);
+    tree1.addNode(13);
+    tree1.addNode(11);
+
+    tree1.printTree(tree1.getRoot());
+    System.out.println("Searching for 8...");
+    System.out.println(tree1.searchNode(8));
+    System.out.println("Searching for 13...");
+    System.out.println(tree1.searchNode(13));
+    System.out.println("Searching for 15...");
+    System.out.println(tree1.searchNode(15));*/
   }
+}
--- Trees/Node.java
+++ Trees/Node.java
@@ -11,21 +11,43 @@ public class Node {
   public Node parent;
   public Node child1;
   public Node child2;
+  public Integer val;
 
-  public Node(Int val) {
+  public Node(int val) {
+    this.val = val;
+    this.parent = null;
+    this.child1 = null;
+    this.child2 = null;
+  }
 
+  public int getValue() {
+    return this.val;
+  }
+
+  public void setParent(Node parent) {
+    this.parent = parent;
   }
 
   public Node getParent() {
-
+    return this.parent;
   }
 
-  public void setChild() {
-
+  public void setChild(Node child) {
+    if (child.getValue() >= this.val) {
+      this.child2 = child;
+    }
+    else {
+      this.child1 = child;
+    }
   }
 
-  public Node getChild() {
-    
+  public Node getChild(boolean right) {
+    if (right) {
+      return this.child2;
+    }
+    else {
+      return this.child1;
+    }
   }
 
 }

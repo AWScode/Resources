593bc47d4dcaf7d6dc684fab1c84012c4cbe7f4a
Zack Considine <zack_considine@m013391.aw.org>
2018-05-07 14:31:31 -0700

    second frame and button actions

--- AWT/MainFrame.java
+++ AWT/MainFrame.java
@@ -12,6 +12,8 @@ public class MainFrame extends Frame{
   private Label name;
   private TextArea ta;
   private Button b2;
+  private Choice c;
+  private Frame f2;
 
   public MainFrame() {
 
@@ -21,9 +23,19 @@ public class MainFrame extends Frame{
       }
     });
 
+    f2 = new Frame();
+    f2.setSize(200,600);
+    f2.setLayout(null);
+    f2.setVisible(true);
+    f2.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent windowEvent){
+        System.exit(0);
+      }
+    });
+
     b = new Button("Click me!");
     b.setBounds(90,100,100,30);
-    add(b);
+    f2.add(b);
 
     tf = new TextField("Welcome to the Restaurant!");
     tf.setBounds(50, 50, 200, 30);
@@ -36,15 +48,24 @@ public class MainFrame extends Frame{
       }
     });
 
-    Scrollbar sc = new Scrollbar();
-    sc.setBounds(400,0,30,300);
-    add(sc);
+    //Scrollbar sc = new Scrollbar();
+    //sc.setBounds(400,0,30,300);
+    //add(sc);
     name = new Label("Your name:");
     name.setBounds(70, 250, 80, 30);
     add(name);
     ta = new TextArea();
     ta.setBounds(150,250,200,40);
     add(ta);
+
+    c = new Choice();
+    c.setBounds(5, 120, 75, 180);
+    c.add("10%");
+    c.add("12.5%");
+    c.add("15%");
+    c.add("17.5%");
+    c.add("20%");
+    add(c);
     //ta.addTextListener(new MyTextListener());
     b2 = new Button("Submit");
     b2.setBounds(150, 200, 100, 30);
@@ -74,17 +95,11 @@ public class MainFrame extends Frame{
     this.add(check2);
   }
 
-  public void addChoice() {
-    Choice c = new Choice();
-    c.setBounds(5, 20, 75, 180);
-    c.add("1");
-    c.add("2");
-    c.add("3");
-    c.add("4");
-    c.add("5");
-    this.add(c);
-  }
+*/public String calcTip(String perc) {
+    return null;
+}
 
+/*
   public void addList() {
     List l = new List(5);
     l.setBounds(320, 60, 45, 180);
@@ -98,5 +113,10 @@ public class MainFrame extends Frame{
 */
   public static void main(String[] args) {
     MainFrame m = new MainFrame();
+
+
+
+
+
   }
 }
